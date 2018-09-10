4c1a35f25ce06ed9d7236ae4a18be8d775015581
Mathias Bjerke <mathbje@gmail.com>
2018-09-10 15:32:09 +0200

    Refactoring. Better error messages

--- Edipost/Shipment/view/adminhtml/web/js/edipost.js
+++ Edipost/Shipment/view/adminhtml/web/js/edipost.js
@@ -3,36 +3,16 @@ require([
     "Edipost_Shipment/js/localprint/localprint"
 ], function ($) {
     "use strict";
+
     var lp = new LocalPrint();
 
-    function debug(msg, type) {
+
+    function message(msg, type) {
         var html_str = '<div class="messages"><div class="message message-' + type + ' type' + '"><div data-ui-id="messages-message-' + type + '">' + msg + '</div></div></div>';
         $('.edipost-wrapper #error-block').html(html_str);
         $('.edipost-wrapper #error-block').show();
     }
 
-    /**
-     * Call this function to start RAW print
-     */
-    function startPrintRaw(zplData, printerName) {
-
-        lp.printRaw(zplData, printerName, function (data) {
-            debug( 'OK', 'success');
-        }, function (data) {
-            debug('Error when printing RAW: ' + data.ErrorText, 'error');
-        });
-    }
-
-    /**
-     * Call this function to start PDF print from url
-     */
-    function startPrintPdf(url, printerName) {
-        lp.printPdf( url, printerName, function(data) {
-            debug( 'OK', 'success');
-        }, function(data) {
-            debug('Error when printing PDF: ' + data.ErrorText, 'error');
-        });
-    }
 
     function base64ToArrayBuffer(base64) {
         var raw = window.atob(base64);
@@ -45,10 +25,13 @@ require([
         return ( array.buffer );
     }
 
+
     $('#edipost-open').on('click', function (e) {
         e.preventDefault();
+
         $('#edipost-open').attr("disabled", true);
         $('body').loader('show');
+
         $.ajax({
             type: "POST",
             url: EDIPOST_OPEN_EDIPOST_AJAX_URL,
@@ -59,21 +42,23 @@ require([
             success: function (data) {
                 $('#edipost-open').attr("disabled", false);
                 $('body').loader('hide');
+
                 if (!(data.error)) {
                     window.open(data.url, '_blank');
-                    debug( 'OK', 'success');
+                    message( 'OK', 'success');
                 } else {
                     console.log(JSON.stringify(data));
-                    debug( JSON.stringify(data), 'error');
+                    message( JSON.stringify(data), 'error');
                 }
             },
             error: function (data) {
                 $('#edipost-open').attr("disabled", false);
                 $('body').loader('hide');
-                debug( JSON.stringify(data), 'error');
+                message( JSON.stringify(data), 'error');
             }
         });
     });
+
 
     $('#edipost-create').on('click', function (e) {
         e.preventDefault();
@@ -82,8 +67,10 @@ require([
         if ($('#edipost_e_alert').is(':checked')) {
             e_alert = 1;
         }
+
         $('#edipost-create').attr("disabled", true);
         $('body').loader('show');
+
         $.ajax({
             type: "POST",
             url: EDIPOST_CREATE_SHIPMENT_AJAX_URL,
@@ -98,40 +85,37 @@ require([
             success: function (data) {
                 $('#edipost-create').attr("disabled", false);
                 $('body').loader('hide');
-                if(!(data.error)){
-                    var link = document.createElement('a'),
-                        pdf = data.pdf,
-                        pdf_raw = data.pdf_raw;
-                    link.href = pdf;
-                    link.download = "etiket.pdf";
 
-                    if(data.product_id == 727){
-                        lp.getVersion(function (data) {
-                            startPrintRaw(pdf_raw, EDIPOST_PRINTER);
+                if( ! data.error ){
+                    var pdf = data.pdf;
+                    var raw = data.pdf_raw;
 
+                    if(data.product_id === 727) {   // PiP
+                        lp.printRaw(raw, EDIPOST_PRINTER, function (data) {
+                            message( 'OK', 'success');
                         }, function (data) {
-                            link.click();
-                            debug( 'OK', 'success');
+                            message('Error when printing RFID label. Error message: ' + data.statusText, 'error');
                         });
-                    } else {
-                        lp.getVersion(function (data) {
-                            startPrintPdf(pdf, EDIPOST_PRINTER);
 
-                        }, function (data) {
-                            link.click();
-                            debug( 'OK', 'success');
+                    } else {    // All other products
+                        lp.printPdf( pdf, EDIPOST_PRINTER, function(data) {
+                            message( 'OK', 'success');
+                        }, function(data) {
+                            message( 'Error when printing PDF, download <a href="' + pdf + '" target="_blank">label</a> manually. Error message: ' + data.statusText );
                         });
                     }
+
                 } else {
-                    console.log(JSON.stringify(data));
+                    message( 'Error when creating consignment. Error message: ' + data.statusText );
                 }
             },
             error: function (data) {
                 $('#edipost-create').attr("disabled", false);
                 $('body').loader('hide');
                 console.log(JSON.stringify(data));
-            },
+            }
         });
     });
 
+    
 });
